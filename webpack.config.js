272f1d7f568ee0ef0a1333105ef0cd2f8ae5a8ad
kidow <wcgo2ling@hanmail.net>
2019-06-30 02:54:38 +0900

    webpack entry, output 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,17 @@
-module.exports = {}
+const path = require('path')
+
+module.exports = {
+  name: 'wordrelay-setting',
+  mode: 'development',
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  entry: {
+    app: ['./client']
+  }, // 입력
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js'
+  } // 출력
+}

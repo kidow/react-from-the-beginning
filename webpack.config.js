474c8a3b072a76181c301ce0ed6f8fd267229afb
kidow <wcgo2ling@hanmail.net>
2019-06-30 11:54:10 +0900

    preset의 설정

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,18 @@ module.exports = {
         test: /\.jsx?/, // js와 jsx 파일에 룰을 적용한다
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  browsers: ['> 1% in KR']
+                },
+                debug: true
+              }
+            ],
+            '@babel/preset-react'
+          ],
           plugins: ['@babel/plugin-proposal-class-properties']
         }
       }
